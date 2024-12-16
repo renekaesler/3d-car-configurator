250d4894aacc146dc91b5c2ef5f09e4c174d0437
Rene Kaesler <renekaesler@gmail.com>
2024-12-16 20:45:57 +0100

    remove top-level await

--- src/main.js
+++ src/main.js
@@ -19,43 +19,51 @@ const getMaterialOptions = () => {
   return options;
 }
 
-const { scenes, cars, wheels, materialPresets } = await Json.fetch('config.json');
-const state = {
-  scene: scenes.at(0),
-  car: cars.at(0),
-  wheels: wheels.at(0),
-  displacement: 0.0,
+(async () => {
+  const { scenes, cars, wheels, materialPresets } = await Json.fetch('config.json');
+  const state = {
+    scene: scenes.at(0),
+    car: cars.at(0),
+    wheels: wheels.at(0),
+    displacement: 0.0,
+  
+    material: '',
+    albedo: [1.0, 1.0, 1.0],
+    alpha: 1.0,
+    metallic: 1.0,
+    roughness:  0.0,
+    occlusion:  1.0,
+  };
 
-  material: '',
-  albedo: [1.0, 1.0, 1.0],
-  alpha: 1.0,
-  metallic: 1.0,
-  roughness:  0.0,
-  occlusion:  1.0,
-};
+  const gui = createGui({ scenes, cars, wheels, state, materialPresets});
 
-const gui = createGui({ scenes, cars, wheels, state, materialPresets});
+  gui.onChange(async ({ property, value }) => {
+    switch(property) {
+      case 'displacement':
+        configurator.car.displace(value);
+        break;
+      case 'scene':
+        configurator.loadScene(value);
+        break;
+      case 'car':
+        await configurator.loadCar(value);
+        gui.refreshMaterialOptions(getMaterialOptions());
+        break;
+      case 'wheels':
+        await configurator.loadWheels(value);
+        gui.refreshMaterialOptions(getMaterialOptions());
+        break;
+    }
+  });
 
-gui.onChange(async ({ property, value }) => {
-  switch(property) {
-    case 'displacement':
-      configurator.car.displace(value);
-      break;
-    case 'scene':
-      configurator.loadScene(value);
-      break;
-    case 'car':
-      await configurator.loadCar(value);
-      gui.refreshMaterialOptions(getMaterialOptions());
-      break;
-    case 'wheels':
-      await configurator.loadWheels(value);
-      gui.refreshMaterialOptions(getMaterialOptions());
-      break;
-  }
-});
+  await configurator.run(state);
+  gui.refreshMaterialOptions(getMaterialOptions());
 
-await configurator.run(state);
-gui.refreshMaterialOptions(getMaterialOptions());
 
-document.body.prepend(configurator.canvas);
+  document.body.prepend(configurator.canvas);
+})()
+
+
+
+
+
